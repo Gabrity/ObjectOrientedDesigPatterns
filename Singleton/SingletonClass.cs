000c3f8460e94258e7b929d59916420acf79e8ce
Gabrity, G. (Gabor) <mp26od@INSIM.BIZ>
2019-06-03 23:35:08 +0200

    Add singleton pattern

---
+++ Singleton/SingletonClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class SingletonClass
+    {
+        private static SingletonClass Instance;
+        private SingletonClass()
+        {
+        }
+
+        public static SingletonClass GetInstance()
+        {
+            // lazy instantiation
+            if (Instance == null)
+            {
+                return new SingletonClass();
+            }
+
+            return Instance;
+        }
+    }
+}
