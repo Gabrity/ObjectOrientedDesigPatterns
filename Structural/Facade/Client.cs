12b8b66c33694fc73b920b7b81a709516e58ca8a
Gabrity, G. (Gabor) <mp26od@INSIM.BIZ>
2019-07-25 08:36:16 +0200

    Initial setup for Facede

---
+++ Structural/Facade/Client.cs
@@ -0,0 +1,90 @@
+﻿namespace Facade
+{
+    public class Client
+    {
+        private readonly IBankAccount _bankAccount;
+        public Client()
+        {
+            var _bankAccount = new BankAccount();
+        }
+
+        public void Manipulate()
+        {
+            _bankAccount.DepositCash(5000.0);
+            _bankAccount.WithdrawCash(300.0);
+        }
+    }
+
+    public class BankAccount : IBankAccount
+    {
+        private double _balance;
+        private readonly IBalanceValidator _balancevalidator;
+        private readonly IPinCodeValidator _pinCodeValidator;
+        private readonly ICardReaderValidator _cardReaderValidator;
+        
+        public BankAccount()
+        {
+            _balance = 0.0;
+            _balancevalidator = new BalanceValidator();
+            _pinCodeValidator = new PinCodeValidator();
+            _cardReaderValidator = new CardReaderValidator();
+        }
+
+        public void WithdrawCash(double requestedAmount)
+        {
+            _pinCodeValidator.Validate();
+            _cardReaderValidator.Validate();
+            _balancevalidator.Validate(_balance, requestedAmount);
+        }
+
+        public void DepositCash(double amount)
+        {
+            _balance = _balance + amount;
+        }
+    }
+
+    public class BalanceValidator : IBalanceValidator
+    {
+        public void Validate(double balance, double requestedAmount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    internal interface IBalanceValidator
+    {
+        void Validate(double balance, double requestedAmount);
+    }
+
+    public class CardReaderValidator : ICardReaderValidator
+    {
+        public void Validate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class PinCodeValidator : IPinCodeValidator
+    {
+        public void Validate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    internal interface ICardReaderValidator
+    {
+        void Validate();
+    }
+
+    internal interface IPinCodeValidator
+    {
+        void Validate();
+    }
+
+    public interface IBankAccount
+    {
+        void WithdrawCash(double amount);
+        void DepositCash(double amount);
+    }
+}
---
+++ Structural/Facade/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    /* Facade: Provide a unified interface to a set of interfaces in a subsystem. Facade defines a higher-level interface that makes the subsystem easier to use.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
