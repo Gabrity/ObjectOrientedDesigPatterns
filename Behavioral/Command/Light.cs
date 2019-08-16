6b9811b689fbe0db77ac60c11e4e608af0c2e6fa
Gabrity, G. (Gabor) <mp26od@INSIM.BIZ>
2019-08-17 00:43:55 +0200

    Add command design pattern

---
+++ Behavioral/Command/Light.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Command
+{
+    public class Light
+    {
+        private readonly string _name;
+
+        public Light(string name)
+        {
+            _name = name;
+        }
+
+        internal void On()
+        {
+            Console.WriteLine($"{_name} Light On");
+        }
+
+        internal void Off()
+        {
+            Console.WriteLine($"{_name} Light Off");
+        }
+    }
+}
---
+++ Behavioral/Command/OnOffStruct.cs
@@ -0,0 +1,8 @@
+﻿namespace Command
+{
+    internal struct OnOffStruct
+    {
+        public ICommand On;
+        public ICommand Off;
+    }
+}
